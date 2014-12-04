d5568cfa26c6f702ae60c7b98722d9c07bbc07c8
Ed Tsech <edtsech@gmail.com>
2014-12-04 22:24:22 +0100

    Format tests

--- test/fun_test.js
+++ test/fun_test.js
@@ -31,54 +31,54 @@ f2.define(undefined, function(t) {
   return 'undefined';
 })
 
-    test('takes default implementation', function(t) {
-      t.plan(1);
-      var result = f(123)
-      t.equal(result, 123)
-    })
+test('takes default implementation', function(t) {
+  t.plan(1);
+  var result = f(123)
+  t.equal(result, 123)
+})
 
-    test('support native types', function(t) {
-      t.plan(1);
-      var result = f2(123)
-      t.equal(result, 'number')
-    })
+test('support native types', function(t) {
+  t.plan(1);
+  var result = f2(123)
+  t.equal(result, 'number')
+})
 
-    test('supports object wrappers', function(t) {
-      t.plan(1);
-      var result = f2(new String('123'))
-      t.equal(result, 'string')
-    })
+test('supports object wrappers', function(t) {
+  t.plan(1);
+  var result = f2(new String('123'))
+  t.equal(result, 'string')
+})
 
-    test('supports custom types', function(t) {
-      t.plan(1);
-      var result = f2(new Rabbit())
-      t.equal(result, 'rabbit')
-    })
+test('supports custom types', function(t) {
+  t.plan(1);
+  var result = f2(new Rabbit())
+  t.equal(result, 'rabbit')
+})
 
-    test('supports null', function(t) {
-      t.plan(1);
-      var result = f2(null)
-      t.equal(result, 'null')
-    })
+test('supports null', function(t) {
+  t.plan(1);
+  var result = f2(null)
+  t.equal(result, 'null')
+})
 
-    test('supports undefined', function(t) {
-      t.plan(1);
-      var result = f2(undefined);
-      t.equal(result, 'undefined')
-    })
+test('supports undefined', function(t) {
+  t.plan(1);
+  var result = f2(undefined);
+  t.equal(result, 'undefined')
+})
 
-    test('supports functions without default implementation', function(t) {
-      t.plan(1);
-      var f = fun()
-      t.equal(1,1)
-    })
+test('supports functions without default implementation', function(t) {
+  t.plan(1);
+  var f = fun()
+  t.equal(1,1)
+})
 
-  test('.isSupported returns true if current type is supported', function(t) {
-      t.plan(1);
-    t.equal(f2.isSupported(String), true)
-  })
+test('.isSupported returns true if current type is supported', function(t) {
+    t.plan(1);
+  t.equal(f2.isSupported(String), true)
+})
 
-  test('.hasDefault returns if function has a default implementation', function(t) {
-      t.plan(1);
-    t.equal(f2.hasDefault, true)
-  })
+test('.hasDefault returns if function has a default implementation', function(t) {
+    t.plan(1);
+  t.equal(f2.hasDefault, true)
+})
